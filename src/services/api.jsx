ef75dee215cff5c1a7cf6114f191ecc48b8c7d1f
Priyanshi <priyanshi04@tradeindia.com>
2026-05-05 22:22:32 +0530

    backend-url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    // baseURL: "https://support-system-backend-1.onrender.com/api",
+    baseURL: "https://support-system-backend-1.onrender.com/api",
     headers: {
         "Content-Type": "application/json"
     }
