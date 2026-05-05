f8113f82a2f2f8f53944fea3daef8cd9ca626fd5
Priyanshi <priyanshi04@tradeindia.com>
2026-05-05 15:48:14 +0530

    utf

--- src/pages/TherapistDashboard.jsx
+++ src/pages/TherapistDashboard.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import API from "../services/api";
 import ChatBox from "../components/ChatBox";
 import {
